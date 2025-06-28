5841c5f9cce45eafdc5c459627989994b08f4907
kittt2 <nitinformal3@gmail.com>
2025-06-28 19:20:11 +0530

    api

--- src/pages/Vapi.js
+++ src/pages/Vapi.js
@@ -1,8 +1,8 @@
 import Vapi from '@vapi-ai/web';
 
-const vapi = new Vapi(import.meta.env.VITE_VAPI_API_KEY);
+const vapi = new Vapi(import.meta.env.Vapitokenapi);
 
-vapi.start(import.meta.env.VITE_VAPI_ASSISTANT_ID);
+vapi.start(import.meta.env.VITE_VAPI_ASSI);
 
 vapi.on('message', (message) => {
   if (message.type === 'transcript') {
