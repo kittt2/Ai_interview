31ffacb2ee10b83350c3373df6081c8e7ce50663
kittt2 <nitinformal3@gmail.com>
2025-07-01 23:31:19 +0530

    sss

--- api/feedback.js
+++ api/feedback.js
@@ -8,7 +8,8 @@ const google = createGoogleGenerativeAI({
 });
 
 export default async function handler(req, res) {
-
+res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+// res.setHeader('Access-Control-Allow-Origin', 'https://yourdomain.com');
  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
